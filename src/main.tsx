8ee6d9c3250ffb6d7d51fa345f282593b8034281
Ashot-js <ashotg771@gmail.com>
2026-03-30 02:25:58 +0400

    up myApp

--- src/main.tsx
+++ src/main.tsx
@@ -2,28 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router";
-import { onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
+
 import { store } from "./app/store";
 import { router } from "./app/router";
-import { auth } from "./configs/firebase";
-import { setAuthChecked, setUser } from "./store/authSlice";
+
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
-
-onAuthStateChanged(auth, (firebaseUser) => {
-  if (firebaseUser?.email) {
-    store.dispatch(
-      setUser({
-        id: firebaseUser.uid,
-        email: firebaseUser.email,
-      })
-    );
-  } else {
-    store.dispatch(setUser(null));
-  }
-  store.dispatch(setAuthChecked(true));
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -31,5 +16,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <RouterProvider router={router} />
       <ToastContainer position="top-right" autoClose={3000} />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
