36b2f68f9efd92e1e5f2d7e83df9f79b9a48a248
Ashot-js <ashotg771@gmail.com>
2026-03-29 22:57:07 +0400

    update my App

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,27 +1,18 @@
 
 import { useDispatch, useSelector } from "react-redux";
-
-// Импортируем ТОЛЬКО тип TypedUseSelectorHook (не попадает в JS-бандл)
 import type { TypedUseSelectorHook } from "react-redux";
-
-// Импортируем типы RootState (всё состояние Redux)
-// и AppDispatch (тип dispatch из store)
 import type { RootState, AppDispatch } from "./store";
-
-// Кастомный хук для dispatch
-// Позволяет dispatch'ить экшены с полной типизацией (Thunk, async и т.д.)
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-
-// Кастомный типизированный useSelector
-// Теперь state внутри селекторов будет иметь тип RootState
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-// ❌ Без этого: state и dispatch часто имеют тип any
+// Redux Toolkit (store + slice)
+//         ↓
+// типы (RootState, AppDispatch)
+//         ↓
+// твои хуки (useAppDispatch, useAppSelector)
+//         ↓
+// компоненты React
 
-// ✅ С этим:
+// Простыми словами
 
-// автодополнение в state.auth.user
-
-// ошибки ловятся TypeScript’ом
-
-// идеально для Redux Toolkit
+// умный мост между React и Redux Toolkit
