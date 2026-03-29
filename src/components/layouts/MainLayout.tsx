787ffe374233f010ebf52085201447c6ac3b51f0
Ashot-js <ashotg771@gmail.com>
2026-03-30 02:48:21 +0400

    update App

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,15 +2,8 @@ import { Outlet } from "react-router";
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import "./MainLayout.scss";
-import { useAppSelector } from "../../app/hooks";
 
 const MainLayout = () => {
-  const isAuthChecked = useAppSelector((state) => state.auth.isAuthChecked);
-
-  if (!isAuthChecked) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="MainLayout">
       <Navbar />
