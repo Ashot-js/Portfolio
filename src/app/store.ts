5bd1a3489b51307199f2ca12e0f71885ef13a2d3
Ashot-js <ashotg771@gmail.com>
2026-03-30 02:19:40 +0400

    update

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,10 @@
-
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../store/authSlice";
 
-// Создаём Redux-хранилище
 export const store = configureStore({
-  // Объект reducer объединяет все редьюсеры приложения
   reducer: {
-    // auth — ключ состояния в Redux (state.auth)
-    // authReducer — логика изменения этого состояния
-    auth: authReducer,
+    // пока нет редьюсеров
   },
 });
 
-// Тип RootState автоматически выводит тип всего Redux state.
-// Используется для типизации useSelector.
 export type RootState = ReturnType<typeof store.getState>;
-
-// Тип AppDispatch — тип функции dispatch из текущего store.
-// Используется для типизации useDispatch.
 export type AppDispatch = typeof store.dispatch;
-
-
